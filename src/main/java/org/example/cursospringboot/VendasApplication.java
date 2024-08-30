3b64c020bdb52a81ac854862e27d6a0fa473a684
Welinton Haas <welinton.haas@gmail.com>
2024-08-30 11:11:50 -0300

    Requisição GET com Parâmetros e Response Entity

--- src/main/java/org/example/cursospringboot/VendasApplication.java
+++ src/main/java/org/example/cursospringboot/VendasApplication.java
@@ -1,13 +1,31 @@
 package org.example.cursospringboot;
 
+import org.example.cursospringboot.domain.entity.Cliente;
+import org.example.cursospringboot.domain.repository.ClientesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication
 {
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired ClientesRepository clientesRepository){
+		return args -> {
+			Cliente c = new Cliente(null, "Joaquim");
+			clientesRepository.save(c);
+
+			Cliente c1 = new Cliente(null, "Tom");
+			clientesRepository.save(c1);
+
+			Cliente c2 = new Cliente(null, "Fulano");
+			clientesRepository.save(c2);
+		};
+	}
 
 	public static void main(String[] args)
 	{
--- src/main/java/org/example/cursospringboot/rest/controller/ClienteController.java
+++ src/main/java/org/example/cursospringboot/rest/controller/ClienteController.java
@@ -1,22 +1,33 @@
 package org.example.cursospringboot.rest.controller;
 
 import org.example.cursospringboot.domain.entity.Cliente;
+import org.example.cursospringboot.domain.repository.ClientesRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(
-            value = "/hello/{nome}",
-            method = RequestMethod.POST,
-            consumes = {"application/json"},
-            produces = {"application/json"}
-    )
+    private ClientesRepository clientes;
+
+    private ClienteController(ClientesRepository clientesRepository) {
+        this.clientes = clientesRepository;
+    }
+
+    @GetMapping("/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nome, @RequestBody Cliente cliente) {
-        return String.format("Olá, %s!", nome);
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
